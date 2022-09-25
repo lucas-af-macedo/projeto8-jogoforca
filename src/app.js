91ce9ae46bb10a7a3baaa7d38ef26a01b89dd090
Lucas Macedo <lucasamacedo@hotmail.com>
2022-09-25 20:36:27 -0300

    input de chute adicionado

--- src/app.js
+++ src/app.js
@@ -7,17 +7,17 @@ let underlineWordNow
 let letterList;
 let rightLettersList;
 let arrayHability = [];
+let wordInput = ''
 let alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 alfabeto.map(()=>{arrayHability.push('habilitado')})
 export default function App(){
-    const [word,setWord]= React.useState('')
     const [wordInGame,setWordInGame]= React.useState('')
     const [buttonCondition,setButtonCondition] = React.useState('')
     const [newGame,setNewGame] = React.useState(0)
     const [image,setImage] = React.useState("./assets/img/forca0.png")
-    const [letterButtons,setLetterButtons] = React.useState(listaLetras)
     const [disability,setDisability] = React.useState(true)
     const [color,setColor] = React.useState('')
+    const [tryWord,setTryWord] = React.useState('')
     function listaLetras(){
         return(
             <>
@@ -33,19 +33,86 @@ export default function App(){
         </>)
     }
 
-    function testWin(wordArray1,wordArray2){
+    function testWin(){
         let index=0
-        while (index<wordArray1.length){
-            if(wordArray1[index]!==wordArray2[index])
+        while (index<letterList.length){
+            if(letterList[index]!==rightLettersList[index])
                 return(0)
             index+=1
         }
-        console.log(wordArray1)
-        console.log(wordArray2)
+        console.log(letterList)
+        console.log(rightLettersList)
         return (1)
     }
 
-    function testLetter(letter){
+    function tryWin(){
+        let index = 0
+        let regex
+        let win =1
+        wordInput=tryWord
+        if (tryWord.length<rightLettersList.length){
+            for(let i=0;i<(rightLettersList.length-tryWord.length);i++){
+                wordInput=wordInput+'_'
+            }
+        }
+        while(index<rightLettersList.length){
+            switch (wordInput[index]){
+                case 'a':
+                    regex = /^[aáàâã ]+$/i
+                    if(!letterList[index].match(regex))
+                            win=0
+                        break
+                    case 'e':
+                        regex = /^[eéèê ]+$/i
+                        if(!letterList[index].match(regex))
+                            win=0
+                        break
+                    case 'i':
+                        regex = /^[iíï ]+$/i
+                        if(!letterList[index].match(regex))
+                            win=0
+                        break
+                    case 'u':
+                        regex = /^[uú ]+$/i
+                        if(!letterList[index].match(regex))
+                            win=0
+                        break
+                    case 'o':
+                        regex = /^[oóôõö ]+$/i
+                        if(!letterList[index].match(regex))
+                            win=0
+                        break
+                    case 'c':
+                        regex = /^[cç ]+$/i
+                        if(!letterList[index].match(regex))
+                            win=0
+                        break
+                    default:
+                        if(letterList[index]!==wordInput[index])
+                            win=0
+                        break
+            }
+            index+=1
+        }
+        if (win===0){
+            setNewGame(6)
+            setImage("./assets/img/forca6.png")
+            setDisability(true)
+            setButtonCondition('')
+            setColor('lose')
+            setWordInGame(letterList.map((f, index)=> (<li key ={index}><RenderWord character={f} index={index}/></li>)))
+        }else{
+            setDisability(true)
+            setButtonCondition('')
+            setColor('win')
+            setWordInGame(letterList.map((f, index)=> (<li key ={index}><RenderWord character={f} index={index}/></li>)))
+        }
+        console.log(wordInput)
+        setTryWord('')
+
+    }
+
+    function testRegex(letter){
         let regex
         let test = 0
         switch (letter){
@@ -105,6 +172,10 @@ export default function App(){
             })
                 break
         }
+        return test
+    }
+    function testLetter(letter){
+        let test = testRegex(letter)
         if (test === 1){
             setWordInGame(rightLettersList.map((f, index)=> (<li key ={index}><RenderWord character={f} index={index}/></li>)))
 
@@ -118,7 +189,7 @@ export default function App(){
                 setWordInGame(letterList.map((f, index)=> (<li key ={index}><RenderWord character={f} index={index}/></li>)))
             }
         }
-        if (testWin(letterList,rightLettersList)){
+        if (testWin()){
             setDisability(true)
             setButtonCondition('')
             setColor('win')
@@ -146,9 +217,9 @@ export default function App(){
 
     function wordSelector(){
         wordNow = palavras[Math.round(Math.random()*palavras.length)]
-        setWord(palavras[0])
         condicao = 'habilitado'
         letterList=wordNow.split('')
+        console.log(letterList)
         rightLettersList=letterList.map(()=>'_')
         setWordInGame(rightLettersList.map((f, index)=> (<li key ={index}><RenderWord character={f} index={index}/></li>)))
         setButtonCondition('habili')
@@ -157,6 +228,7 @@ export default function App(){
         setNewGame(0)
         setDisability(false)
         setColor('')
+        wordInput=''
     }
     return(
         <div>
@@ -176,8 +248,8 @@ export default function App(){
             </div>
             <div className="chute">
                 <p>Ja sei a palavra!</p>
-                <input disabled={disability}></input>
-                <button><strong>Chutar</strong></button>
+                <input value={tryWord} onChange={e => setTryWord(e.target.value.toLowerCase())} disabled={disability}></input>
+                <button onClick={tryWin} disabled={disability}><strong>Chutar</strong></button>
             </div>
         </div>
     )
