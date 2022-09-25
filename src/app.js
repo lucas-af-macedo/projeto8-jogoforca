7bac4e18b97abdb3a3099fda62324dd2f1f40321
Lucas Macedo <lucasamacedo@hotmail.com>
2022-09-25 17:25:37 -0300

    escolher palavra e precionar uma letra adicionados

--- src/app.js
+++ src/app.js
@@ -1,40 +1,140 @@
 import palavras from "./palavras"
 import React from 'react';
-function BotaoAlfabeto(props){
-    return(
-        <button ><strong>{props.letra.toUpperCase()}</strong></button>
-    )
-}
+
+let condicao = 'desabilitado'
+let wordNow
+let underlineWordNow
+let letterList;
+let rightLettersList;
+let arrayHability = [];
+let alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alfabeto.map(()=>{arrayHability.push('habilitado')})
 export default function App(){
-    const [word,setWord]= React.useState('')
     const [wordInGame,setWordInGame]= React.useState('')
+    const [buttonCondition,setButtonCondition] = React.useState('')
+    const [newGame,setNewGame] = React.useState(0)
+    const [image,setImage] = React.useState("./assets/img/forca0.png")
+    function RenderWord(props){
+
+        return(
+        <>
+            {props.character}
+        </>)
+    }
+
+    function testLetter(letter){
+        let regex
+        let test = 0
+        switch (letter){
+            case 'a':
+                regex = /^[aáàâã ]+$/i
+                letterList.map((f, index)=>{if(f.match(regex)){
+                    test=1
+                    rightLettersList[index]=f
+                }
+            })
+                break
+            case 'e':
+                regex = /^[eéèê ]+$/i
+                letterList.map((f, index)=>{if(f.match(regex)){
+                    test=1
+                    rightLettersList[index]=f
+                }
+            })
+                break
+            case 'i':
+                regex = /^[iíï ]+$/i
+                letterList.map((f, index)=>{if(f.match(regex)){
+                    test=1
+                    rightLettersList[index]=f
+                }
+            })
+                break
+            case 'u':
+                regex = /^[uú ]+$/i
+                letterList.map((f, index)=>{if(f.match(regex)){
+                    test=1
+                    rightLettersList[index]=f
+                }
+            })
+                break
+            case 'o':
+                regex = /^[oóôõö ]+$/i
+                letterList.map((f, index)=>{if(f.match(regex)){
+                    test=1
+                    rightLettersList[index]=f
+                }
+            })
+                break
+            case 'c':
+                regex = /^[cç ]+$/i
+                letterList.map((f, index)=>{if(f.match(regex)){
+                    test=1
+                    rightLettersList[index]=f
+                }
+            })
+                break
+            default:
+                letterList.map((f, index)=>{if(f===letter){
+                    test=1
+                    rightLettersList[index]=f
+                }
+            })
+                break
+        }
+        if (test === 1){
+            setWordInGame(rightLettersList.map((f, index)=> (<li key ={index}><RenderWord character={f} index={index}/></li>)))
+
+        }else{
+            setNewGame(newGame+1)
+            setImage("./assets/img/forca"+(newGame+1)+".png")
+        }
+    }
+
+
+
+    function BotaoAlfabeto(props){
+        const [letterClicked,setLetterClicked] = React.useState(arrayHability[props.index])
+        function letterSelected(){
+            if (condicao==='habilitado'){
+                arrayHability[props.index]=''
+                if(letterClicked!=='')
+                    testLetter(props.letra)
+                setLetterClicked('')
+            }
+        }
+        
+        return(
+            <button className={letterClicked} onClick={letterSelected}><strong>{props.letra.toUpperCase()}</strong></button>
+        )
+    }
+
+
     function wordSelector(){
-        const wordInFunction = palavras[Math.round(Math.random()*palavras.length)]
-        setWord(wordInFunction)
-        let wordList=wordInFunction.split('')
-        let word2 = ''
-        console.log(wordInFunction)
-        setWordInGame(wordList.map((f, index)=> <li key ={index}>_</li>))
+        wordNow = palavras[Math.round(Math.random()*palavras.length)]
+        condicao = 'habilitado'
+        letterList=wordNow.split('')
+        rightLettersList=letterList.map(()=>'_')
+        setWordInGame(rightLettersList.map((f, index)=> (<li key ={index}><RenderWord character={f} index={index}/></li>)))
+        setButtonCondition('habili')
+        setImage("./assets/img/forca0.png")
+        alfabeto.map((f, index)=>{arrayHability[index]='habilitado'})
+        setNewGame(0)
     }
-    function teste(){
-        alfabeto.pop(0)
-        console.log(alfabeto)
-    }
-    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     return(
         <div>
             <div className="game">
                 <div className="hangman">
-                    <img onClick={teste} src="./assets/img/forca0.png" alt="Imagem não encontrada"/>
+                    <img  src={image} alt="Imagem não encontrada"/>
                 </div>
                 <div className="wordSelector">
                     <button onClick={wordSelector}><strong>Escolher palavra</strong></button>
-                    <ul className="wordInGame">{wordInGame}</ul>
+                    <ul className={"wordInGame"}>{wordInGame}</ul>
                 </div>
             </div>
             <div>
-                <ul className="letterButtons">
-                 {alfabeto.map((f, index) => <li key={index}><BotaoAlfabeto letra={f} key={index} /></li>)}
+                <ul className={"letterButtons "+buttonCondition}>
+                 {alfabeto.map((f, index) => <li key={index} ><BotaoAlfabeto letra={f} index={index}/></li>)}
                 </ul>
             </div>
             <div className="chute">
